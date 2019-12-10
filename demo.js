da402f1f6943d44c5fa1be8daea8d66c31f7a1b3
avinash patil <avinash.patil@in.ncs-i.com>
2019-12-10 21:43:18 +0800

    demo.js

--- demo.js
+++ demo.js
@@ -1,3 +1,59 @@
-setTimeout(function(){
-    console.log('Hey im running function');
-},2000);
+
+
+
+
+
+/*****************************Function Expressions****************************** */
+// lambda function statement
+
+// var Name = ()=>{
+//     console.log('Hi, I am Avinash....');
+//    }
+   
+// Name();
+
+// function expression statement
+
+// var Name = function(){
+//     console.log('Hi, I am Avinash....');
+// }
+
+// function callFunction(name){
+// console.log(typeof(name)+'\n'+name);
+// name();
+// }
+
+// callFunction(Name);
+
+
+// var Name = function (){
+//     console.log('Hi, I am Avinash....');
+// }
+
+// Name();
+
+
+//normal function statement {box function}
+
+// function avinash(){console.log('Hi, My name is Avinash....');
+// }
+
+// avinash();
+
+
+
+
+/*****************************Global Object****************************** */
+// console.log(__dirname);
+// console.log(__filename);
+// var time = 0;
+// var Timer = setInterval(
+        
+//  function(){
+
+//     time  +=  2;
+//         if(time>5){
+//             clearTimeout(Timer);                 
+//         }   
+//         console.log(time + 'Hey im running function');    
+//     },2000);
